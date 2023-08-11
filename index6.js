ac5016b4c7c69b6c3255908a92bd067e2024c0f7
Andres David Restrepo Hernandez <adrestreph@eafit.edu.co>
2023-08-10 21:42:29 -0500

    Ejercicio 6 - Modificar con operadores asignados

---
+++ index6.js
@@ -0,0 +1,7 @@
+alert("Modificar con un numero con operadores de asignacion: ");
+var numero = prompt("Ingrese el numero que desea modificar");
+
+parseFloat(numero);
+numero *= 2;
+numero -= 2;
+numero += numero;
