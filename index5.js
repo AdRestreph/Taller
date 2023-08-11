0ff4a382edd18624bb8dac5db52d7c0be0ebbaa4
Andres David Restrepo Hernandez <adrestreph@eafit.edu.co>
2023-08-10 21:28:08 -0500

    Ejercicio 5 - Operadores de asignacion x2

---
+++ index5.js
@@ -0,0 +1,7 @@
+alert("Multiplicacion x2 con operadores de asignacion");
+var numero = prompt("Numero que desea multiplicar");
+
+parseInt(numero);
+numero *= 2;
+
+alert("El numero resultado es: " + numero);
